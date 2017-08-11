2ed7cc0f65e5e64c4104533fe77cf5f7a69c4580
oharsta <oharsta@zilverline.com>
2017-08-11 11:04:03 +0200

    Quick fix for new style ARP

--- src/main/java/me/model/ArpUnserializer.java
+++ src/main/java/me/model/ArpUnserializer.java
@@ -4,9 +4,13 @@ import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class ArpUnserializer {
 
@@ -22,6 +26,13 @@ public class ArpUnserializer {
     while (matcher.find()) {
       result.add(matcher.group(1));
     }
-    return Optional.of(result);
+      Set<String> attributes = result.stream()
+          .filter(attribute -> attribute.startsWith("urn"))
+          .collect(Collectors.toSet());
+
+      List<String> sorted = new ArrayList<>(attributes);
+      Collections.sort(sorted);
+
+      return Optional.of(sorted);
   }
 }
--- src/test/java/me/model/ArpUnserializerTest.java
+++ src/test/java/me/model/ArpUnserializerTest.java
@@ -4,8 +4,13 @@ import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -29,13 +34,29 @@ public class ArpUnserializerTest {
         "\"urn:mace:terena.org:attribute-def:schacHomeOrganization\";a:1:{i:0;s:1:\"*\";}s:47:" +
         "\"urn:mace:dir:attribute-def:eduPersonAffiliation\";a:1:{i:0;s:1:\"*\";}}";
     Collection<String> attributes = subject.unserialize(arp).get();
-    assertEquals(asList(
-        "urn:mace:dir:attribute-def:eduPersonPrincipalName",
-        "urn:mace:dir:attribute-def:displayName",
-        "urn:mace:dir:attribute-def:cn",
-        "urn:mace:dir:attribute-def:mail",
-        "urn:mace:terena.org:attribute-def:schacHomeOrganization",
-        "urn:mace:dir:attribute-def:eduPersonAffiliation"), attributes);
+
+      List<String> expected = asList(
+          "urn:mace:dir:attribute-def:eduPersonPrincipalName",
+          "urn:mace:dir:attribute-def:displayName",
+          "urn:mace:dir:attribute-def:cn",
+          "urn:mace:dir:attribute-def:mail",
+          "urn:mace:terena.org:attribute-def:schacHomeOrganization",
+          "urn:mace:dir:attribute-def:eduPersonAffiliation");
+
+      Collections.sort(expected);
+      List<String> sorted = new ArrayList<>(attributes);
+      Collections.sort(sorted);
+      assertEquals(expected, sorted);
+  }
+
+  @Test
+  public void testNewArpStyle() throws Exception {
+      String arp = "a:7:{s:46:\"urn:mace:dir:attribute-def:eduPersonTargetedID\";a:1:{i:0;a:1:{s:5:\"value\";s:1:\"*\";}}s:38:\"urn:mace:dir:attribute-def:displayName\";a:1:{i:0;a:1:{s:5:\"value\";s:1:\"*\";}}s:29:\"urn:mace:dir:attribute-def:cn\";a:1:{i:0;a:1:{s:5:\"value\";s:1:\"*\";}}s:29:\"urn:mace:dir:attribute-def:sn\";a:1:{i:0;a:1:{s:5:\"value\";s:1:\"*\";}}s:31:\"urn:mace:dir:attribute-def:mail\";a:1:{i:0;a:1:{s:5:\"value\";s:1:\"*\";}}s:47:\"urn:mace:dir:attribute-def:eduPersonAffiliation\";a:1:{i:0;a:1:{s:5:\"value\";s:1:\"*\";}}s:30:\"urn:mace:dir:attribute-def:uid\";a:1:{i:0;a:1:{s:5:\"value\";s:1:\"*\";}}}";
+      Collection<String> attributes = subject.unserialize(arp).get();
+      int size = attributes.size();
+
+      Set<String> uniqueValues = new HashSet<>(attributes);
+      assertEquals(size, uniqueValues.size());
   }
 
   @Test
